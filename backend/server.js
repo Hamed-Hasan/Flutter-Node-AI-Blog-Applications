5e909ea080893901a745ebec410b3209871d2f15
Hamed Hasan <hamedhasan422421@gmail.com>
2024-04-09 22:25:47 +0300

    update user role

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swaggerConfig');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Define routes
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
